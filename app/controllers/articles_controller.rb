9ccffc673efcaa85b04202180c38b180cfa37f06
snayrouz <samuel.nayrouz@gmail.com>
2017-08-26 13:36:28 -0600

    Add flash message to update, create and delete methods and add to application layout

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,11 +18,15 @@ class ArticlesController < ApplicationController
 
     @article.save
 
+    flash.notice = "Article '#{@article.title}' Created!"
+
     redirect_to article_path(@article)
   end
 
   def destroy
     @article.destroy
+
+    flash.notice = "Article '#{@article.title}' Deleted!"
 
     redirect_to articles_path
   end
@@ -32,6 +36,8 @@ class ArticlesController < ApplicationController
 
   def update
     @article.update(article_params)
+
+    flash.notice = "Article '#{@article.title}' Updated!"
 
     redirect_to article_path(@article)
   end
