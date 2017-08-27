80dd27b775ff4cf1e29a7645494f41f8442029da
snayrouz <samuel.nayrouz@gmail.com>
2017-08-27 12:03:30 -0600

    Add attachment model and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170827164728) do
+ActiveRecord::Schema.define(version: 20170827175830) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 20170827164728) do
     t.string "image_content_type"
     t.integer "image_file_size"
     t.datetime "image_updated_at"
+  end
+
+  create_table "attachments", force: :cascade do |t|
+    t.bigint "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_attachments_on_article_id"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -50,6 +57,7 @@ ActiveRecord::Schema.define(version: 20170827164728) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "attachments", "articles"
   add_foreign_key "comments", "articles"
   add_foreign_key "taggings", "articles"
   add_foreign_key "taggings", "tags"
